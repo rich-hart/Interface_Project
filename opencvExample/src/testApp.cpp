43831736792f1f9387f46b0b86d0dd301d18762f
Rich Hart <RichHOHart@gmail.com>
2013-10-27 22:10:19 -0400

    Window Resizing working. Kind of....

--- opencvExample/src/testApp.cpp
+++ opencvExample/src/testApp.cpp
@@ -5,7 +5,10 @@
  640 x 480 at 60 frames/second
  320 x 240 at 120 frames/second
  */
-
+extern const int width = 640;
+extern const int height = 480;
+extern const int window_width=320;
+extern const int window_height=240;
 //--------------------------------------------------------------
 void testApp::setup(){
 
@@ -13,16 +16,16 @@ void testApp::setup(){
         vidGrabber.setVerbose(true);
         vidGrabber.setDeviceID(1); //MacBook Pro Camera:0, USB Camera:1
         vidGrabber.setDesiredFrameRate(60);
-        vidGrabber.initGrabber(320,240);
+        vidGrabber.initGrabber(width,height);
 	#else
         vidPlayer.loadMovie("Eye_Med_Movement_Lights.mov");
         vidPlayer.play();
 	#endif
 
-    colorImg.allocate(320,240);
-	grayImage.allocate(320,240);
-	grayBg.allocate(320,240);
-	grayDiff.allocate(320,240);
+    colorImg.allocate(width,height);
+	grayImage.allocate(width,height);
+	grayBg.allocate(width,height);
+	grayDiff.allocate(width,height);
 
 	bLearnBakground = true;
 	threshold = 80;
@@ -30,6 +33,8 @@ void testApp::setup(){
 
 //--------------------------------------------------------------
 void testApp::update(){
+
+    
 	ofBackground(100,100,100);
 
     bool bNewFrame = false;
@@ -45,11 +50,17 @@ void testApp::update(){
 	if (bNewFrame){
 
 		#ifdef _USE_LIVE_VIDEO
-            colorImg.setFromPixels(vidGrabber.getPixels(), 320,240);
+            colorImg.setFromPixels(vidGrabber.getPixels(), width,height);
 	    #else
-            colorImg.setFromPixels(vidPlayer.getPixels(), 320,240);
+            colorImg.setFromPixels(vidPlayer.getPixels(), width,height);
         #endif
-
+        
+        colorImg.resize(window_width,window_height);
+        grayImage.resize(window_width,window_height);
+        grayBg.resize(window_width,window_height);
+        grayDiff.resize(window_width,window_height);
+        
+        
         grayImage = colorImg;
 		if (bLearnBakground == true){
 			grayBg = grayImage;		// the = sign copys the pixels from grayImage into grayBg (operator overloading)
@@ -62,7 +73,9 @@ void testApp::update(){
 
 		// find contours which are between the size of 20 pixels and 1/3 the w*h pixels.
 		// also, find holes is set to true so we will get interior contours as well....
-		contourFinder.findContours(grayDiff, 20, (340*240)/3, 10, true);	// find holes
+		contourFinder.findContours(grayDiff, 20, ((width+20)*height)/3, 10, true);	// find holes
+        
+        
 	}
 
 }
@@ -70,34 +83,37 @@ void testApp::update(){
 //--------------------------------------------------------------
 void testApp::draw(){
 
+
+    
 	// draw the incoming, the grayscale, the bg and the thresholded difference
 	ofSetHexColor(0xffffff);
+    
 	colorImg.draw(20,20);
-	grayImage.draw(360,20);
-	grayBg.draw(20,280);
-	grayDiff.draw(360,280);
+	grayImage.draw(window_width+40,20);
+	grayBg.draw(20,window_height+40);
+	grayDiff.draw(window_width+40,window_height+40);
 
 	// then draw the contours:
 
 	ofFill();
 	ofSetHexColor(0x333333);
-	ofRect(360,540,320,240);
+	ofRect(window_width+40,window_height+300,window_width,window_height);
 	ofSetHexColor(0xffffff);
 
 	// we could draw the whole contour finder
-	//contourFinder.draw(360,540);
+	//contourFinder.draw(width+40,540);
 
 	// or, instead we can draw each blob individually from the blobs vector,
 	// this is how to get access to them:
     for (int i = 0; i < contourFinder.nBlobs; i++){
-        contourFinder.blobs[i].draw(360,540);
+        contourFinder.blobs[i].draw(window_width+40,window_height+300);
 		
 		// draw over the centroid if the blob is a hole
 		ofSetColor(255);
 		if(contourFinder.blobs[i].hole){
 			ofDrawBitmapString("hole",
-				contourFinder.blobs[i].boundingRect.getCenter().x + 360,
-				contourFinder.blobs[i].boundingRect.getCenter().y + 540);
+				contourFinder.blobs[i].boundingRect.getCenter().x + window_width+40,
+				contourFinder.blobs[i].boundingRect.getCenter().y + window_height+300);
 		}
     }
 
@@ -119,6 +135,7 @@ void testApp::keyPressed(int key){
 		case ' ':
 			bLearnBakground = true;
 			break;
+        case '=':
 		case '+':
 			threshold ++;
 			if (threshold > 255) threshold = 255;

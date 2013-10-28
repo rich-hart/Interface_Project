0b2cf77a909eb6d491b3ba5809765744cfad18b2
Rich Hart <RichHOHart@gmail.com>
2013-10-27 22:18:23 -0400

    Grayscale threshold working properly

--- opencvExample/src/testApp.cpp
+++ opencvExample/src/testApp.cpp
@@ -68,12 +68,12 @@ void testApp::update(){
 		}
 
 		// take the abs value of the difference between background and incoming and then threshold:
-		grayDiff.absDiff(grayBg, grayImage);
-		grayDiff.threshold(threshold);
+		//grayDiff.absDiff(grayBg, grayImage);
+		grayImage.threshold(threshold);
 
 		// find contours which are between the size of 20 pixels and 1/3 the w*h pixels.
 		// also, find holes is set to true so we will get interior contours as well....
-		contourFinder.findContours(grayDiff, 20, ((width+20)*height)/3, 10, true);	// find holes
+		contourFinder.findContours(grayImage, 20, ((width+20)*height)/3, 10, true);	// find holes
         
         
 	}

a195d45e86e6bb8d76c139a657a0032ed1c6a92b
Rich Hart <RichHOHart@gmail.com>
2013-11-19 11:49:16 -0500

    broken code

--- example-multipleCameras/src/testApp.cpp
+++ example-multipleCameras/src/testApp.cpp
@@ -1,5 +1,8 @@
 #include "testApp.h"
 
+
+unsigned long EYE_CAMERA_ID = 605224960; // Using port A (MacMini Mid 2011) for this example
+unsigned long FRONT_CAMERA_ID = 605290496;
 //--------------------------------------------------------------
 void testApp::setup(){
 	ofSetVerticalSync(true);
@@ -7,18 +10,54 @@ void testApp::setup(){
 	ofSetLogLevel(OF_LOG_VERBOSE);	
 	
 	vector<ofxMacamPs3EyeDeviceInfo*> deviceList = ofxMacamPs3Eye::getDeviceInfoList();
-	
+	/*Enviroment Camera*/
 	for (int i = 0; i < deviceList.size(); i++) {
+       
 		ofxMacamPs3Eye * camera = new ofxMacamPs3Eye();
 		camera->setDeviceID(deviceList[i]->id);
 		camera->setDesiredFrameRate(180);
-		camera->initGrabber(320, 240);
+		camera->initGrabber(CAM_WIDTH, CAM_HEIGHT);
+        camera->setUseTexture(true);
+            
 		cameras.push_back(camera);
         
+         if(deviceList[i]->locationID == FRONT_CAMERA_ID){
+            // vidInput = camera;
+             cout<< "Front Camera: " <<deviceList[i]->locationID<<endl;
+         }
+        
+        if(deviceList[i]->locationID == EYE_CAMERA_ID){
+          //  vidInput = camera;
+            cout<< "Front Camera: " <<deviceList[i]->locationID<<endl;
+        }
 	}
+    
+    
+    // allocate stuff
+	colorImg.allocate(CAM_WIDTH, CAM_HEIGHT);
+	grayImg.allocate(CAM_WIDTH, CAM_HEIGHT);
+    
+	choosing_img = false;
+	chosen_img = false;
+
+    
+    
+    /*Eye Camera*/
+    /*
+    for (int i = 0; i < deviceList.size(); i++) {
+        if(deviceList[i]->locationID == EYE_CAMERA_ID){
+            ofxMacamPs3Eye * camera = new ofxMacamPs3Eye();
+            camera->setDeviceID(deviceList[i]->id);
+            camera->setDesiredFrameRate(180);
+            camera->initGrabber(CAM_WIDTH, CAM_HEIGHT);
+            cameras.push_back(camera);
+            cout<< "Eye Camera: " <<deviceList[i]->locationID<<endl;
+        }
+	}
+     */
 	
 	if(cameras.size() > 0){
-		ofSetWindowShape(320 * cameras.size(), 240);
+		ofSetWindowShape(CAM_WIDTH * cameras.size(), CAM_HEIGHT);
 	}
 }
 
--- example-multipleCameras/src/testApp.h
+++ example-multipleCameras/src/testApp.h
@@ -1,7 +1,26 @@
 #pragma once
 
+//#include "ofxMacamPs3Eye.h"
+
+
+
+
+
+#ifndef _TEST_APP
+#define _TEST_APP
+
 #include "ofMain.h"
-#include "ofxMacamPs3Eye.h"
+
+#include "pkmDetector.h"
+
+#include "ofxOpenCv.h"
+#include "ofShader.h"
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/features2d/features2d.hpp>
+
+const int CAM_WIDTH = 320;
+const int CAM_HEIGHT = 240;
 
 class testApp : public ofBaseApp{
 	public:
@@ -18,6 +37,17 @@ class testApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
-	
-	vector<ofxMacamPs3Eye*> cameras; 
+	/*
+	vector<ofxMacamPs3Eye*> cameras;  //multiple camera example
+    
+    ofxMacamPs3Eye *			vidInput;
+	ofxCvColorImage			colorImg, testImg;
+	ofxCvGrayscaleImage		grayImg, testGrayImg;
+	int						x_start, x_end, y_start, y_end;
+	bool					choosing_img, chosen_img;
+	pkmDetector				detector;
+	int						detection;
+    */
+    
+    
 };
